f760b22271fae070c23bd0752f138b4bc1028589
Gabriel Correa Pereira <gabriel.pereira@nikos.com.br>
2025-08-13 17:27:59 -0300

    novo commit pipeline numero 2

--- automation/inventory.sh
+++ automation/inventory.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ ! -f ../compute/terraform.tfstate ]; then
+# Este script lê o IP público da instância criada pelo Terraform.
+
+if [ ! -f ./compute/terraform.tfstate ]; then
   echo "Terraform state file not found."
   exit 1
 fi
 
-TF_STATE_IP=$(terraform -chdir=../compute output -raw instance_ip)
+TF_STATE_IP=$(terraform -chdir=./compute output -raw instance_ip)
 
 cat <<END_OF_JSON
 {
@@ -15,7 +17,7 @@ cat <<END_OF_JSON
     ],
     "vars": {
       "ansible_user": "ubuntu",
-      "ansible_ssh_private_key_file": "../compute/devops-pdi.pem"
+      "ansible_ssh_private_key_file": "./compute/devops-pdi.pem"
     }
   }
 }
