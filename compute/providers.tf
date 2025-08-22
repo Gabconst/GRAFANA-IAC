e8c3ee9a2144184feca1ec44f64ba0eb840e6b32
Gabriel Correa Pereira <gabriel.pereira@nikos.com.br>
2025-08-22 17:54:55 -0300

    Kaiju n27

--- compute/providers.tf
+++ compute/providers.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.51"
+      version = "~> 5.0
     }
   }
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.5.0
 }
 
 provider "aws" {
