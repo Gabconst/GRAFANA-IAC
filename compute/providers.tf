5c942418a38e2d40f894f1e004fe41014487c304
Gabriel Correa Pereira <gabriel.pereira@nikos.com.br>
2025-08-22 18:05:49 -0300

    Kaiju n29

--- compute/providers.tf
+++ compute/providers.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"   # <-- aspas fechando
+      version = "~> 5.0"
     }
   }
-  required_version = ">= 1.5.0"  # <-- aspas fechando
+  required_version = ">= 1.5.0"
 }
 
 provider "aws" {
-  profile = var.aws_profile
-  region  = var.aws_region
+  region = var.aws_region
+  # profile removido, pois o GitHub Actions usa AWS_ACCESS_KEY_ID e AWS_SECRET_ACCESS_KEY
 }
